0cbd547a5b0b0047a815f35a0127a31a355491b1
Sergio Barrera <sdbeng@gmail.com>
2014-08-27 11:58:56 -0700

    Display model data

--- js/router.js
+++ js/router.js
@@ -3,3 +3,11 @@
 Todos.Router.map(function() {
 	this.resource('todos', {path: '/'});
 });
+
+//Implement a TodosRoute class with a model function that returns all the
+//existing todo - will get rid of the hardcoded data
+Todos.TodosRoute = Ember.Route.extend({
+	model: function() {
+		return this.store.find('todo');
+	}
+});
