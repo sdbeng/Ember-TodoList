9d46bb6bc149e01a9b698cd2f7d5290ef2c195f3
Sergio Barrera <sdbeng@gmail.com>
2014-08-27 11:44:54 -0700

    Add Fixtures: way to put sample data into application before connecting it to long-term persistence

--- js/application.js
+++ js/application.js
@@ -1,3 +1,11 @@
 //Create a new instance of Ember.Application and make it available as a
 //variable named Todos:
 window.Todos = Ember.Application.create();
+/* Then, update the code to indicate that your
+ application's ApplicationAdapter is an extension of the
+  DS.FixtureAdapter. Adapters are responsible for communicating with
+   a source of data for your application. Typically this will be a
+   web service API, but in this case we are using an adapter designed
+   to load fixture data:*/
+
+Todos.ApplicationAdapter = DS.FixtureAdapter.extend();
--- js/models/todo.js
+++ js/models/todo.js
@@ -2,3 +2,22 @@ Todos.Todo = DS.Model.extend({
 	title: DS.attr('string'),
 	isCompleted: DS.attr('boolean')
 });
+
+//Include the following fixture data:
+Todos.Todo.FIXTURES = [
+	{
+		id: 1,
+		title: 'Learn EmberJS and Rails',
+		isCompleted: true
+	},
+	{
+		id: 2,
+		title: 'Brush angularjs skills every week',
+		isCompleted: false
+	},
+	{
+		id: 3,
+		title: 'Push code to github daily',
+		isCompleted: false
+	}
+];
